f71be4a830b4bb1bc7cb134fe6484428fafd7bc9
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-02-23 13:58:26 -0500

    Card update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,18 @@ import './card.css'
 
 export const Card = () => {
   return (
-    <div>
+    <div className='art-card'>
+      <div className='art-card-title'>
+          <label>Title</label>
+      </div>
 
+      <div className='art-card-body'>
+          <label>Body</label>
+      </div>
+
+      <div className='art-card-footer'>
+          <label>Footer</label>
+      </div>
     </div>
   )
 }
