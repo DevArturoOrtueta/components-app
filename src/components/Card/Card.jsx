5ad066ec5ad3fd6a03faed450e3b361b49594ff9
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-02-24 12:12:30 -0500

    Card development

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,68 @@
 import React from 'react'
 import './card.css'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
-    <div className='art-card'>
-      <div className='art-card-title'>
-          <label>Title</label>
-      </div>
+    props?.link_card ? 
+        <a href={props.link_card} className="art-card-ref">
+          <div className='art-card'>
+        { props?.title ? 
+          <div className='art-card-title'>
+            <label>{props.title}</label>
+          </div>
+          :
+          <></>
+        }
 
-      <div className='art-card-body'>
-          <label>Body</label>
-      </div>
+        {
+          props?.image ?
+          <div className='art-card-img'>
+            <img src={props?.image} />
+          </div>
+          : 
+          <></>
+        }
+      
 
-      <div className='art-card-footer'>
-          <label>Footer</label>
+        <div className='art-card-body'>
+            <label>Body</label>
+        </div>
+
+        <div className='art-card-footer'>
+            <label>Footer</label>
+        </div>
       </div>
-    </div>
+        </a>
+      :
+      <div className='art-card'>
+        { props?.title ? 
+          <div className='art-card-title'>
+            <label>{props.title}</label>
+          </div>
+          :
+          <></>
+        }
+
+        {
+          props?.image ?
+          <div className='art-card-img'>
+            <img src={props?.image} />
+          </div>
+          : 
+          <></>
+        }
+      
+
+        <div className='art-card-body'>
+            <label>Body</label>
+        </div>
+
+        <div className='art-card-footer'>
+            <label>Footer</label>
+        </div>
+      </div>
   )
+      
+    
+  
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Card } from './components/Card/Card'
-
+const objeto = [
+  {
+    "id": 1,
+   "title": 'Hola',
+   "image": 'https://firebasestorage.googleapis.com/v0/b/arturoortueta-9b8b4.appspot.com/o/profile.jpg?alt=media&token=66cbad56-4210-4477-9e70-6d4b393eb8b2',
+    "link_card": "https://www.google.com/",
+    "link_ref": ""
+  },
+  {
+    "id": 2,
+    "title": 'Hola 2'
+  }
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card />
+
+    {
+      objeto.map(({title, image, id, link_card}) => (
+        <Card title={title} image={image} key={id} link_card={link_card}/>
+      ))
+    }
+    
   </React.StrictMode>
 )
